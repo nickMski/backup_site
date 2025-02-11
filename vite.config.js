a11ef708c5ede9dec9f9dd5cd50035cbfb54a261
nickMski <murawsk8@uwm.edu>
2025-02-10 18:16:48 -0600

    Update vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  plugins: [react()],
-  base: '/backupSite/',
+  base: '/backup_site/',  // Add this line
+  plugins: [react()]
 })
